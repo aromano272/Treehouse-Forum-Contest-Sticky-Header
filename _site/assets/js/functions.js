dc971841afb25652fe834ec87f92584d61e0467c
aromano272 <andre.romano272@gmail.com>
2014-10-08 14:09:26 +0100

    got the alternate heading right

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,44 +1,55 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
 
-var $header = $("main-header"), 
-    $menu = $("main-nav__wrapper"), 
-    yPos, 
-    yPosDiff = window.pageYOffset, 
+var $header = $(".main-header"), 
+    $menu = $(".main-nav__wrapper"), 
+    yPosDelay, 
+    yPosLive = window.pageYOffset, 
     yPosDiffHolder, 
     headerHeight = $header.outerHeight(),
+    headingHeight = $(".main-header__heading").outerHeight(),
     menuHeight = $menu.outerHeight();
 
 
 
 var yScroll = function() {
-  yPosDiff = window.pageYOffset;
+  yPosLive = window.pageYOffset;
   
-  if(yPos > yPosDiff) {
+  if(yPosDelay > yPosLive) {
     // going up
-    yPosDiffHolder += yPos - yPosDiff;
-  } else if(yPos < yPosDiff) {
+    yPosDiffHolder += yPosDelay - yPosLive;
+  } else if(yPosDelay < yPosLive) {
     // going down
     yPosDiffHolder = 0;
   }
   
-  if(yPosDiffHolder > 120 || window.pageYOffset < 100) {
-    console.log("first if trigger" + " yPos: " + yPos);
-    // show header
+
+  if(yPosLive < headingHeight) {
     $header.removeClass("main-header--hidden");
-
-  } else if(yPos < 150) {
-    console.log("second if trigger" + " yPos: " + yPos);
-    // hide header
-    $header.addClass("main-header--hidden");
-
+    $(".posts").css("padding-top", 0);
   } else {
-    console.log("else trigger" + " yPos: " + yPos);
-    // hide header
     $header.addClass("main-header--hidden");
+    $(".posts").css("padding-top", headerHeight);
+  }
 
-  } 
-  yPos = window.pageYOffset;
+
+  // if(yPosDiffHolder > 120 || window.pageYOffset < 100) {
+  //   console.log("first if trigger" + " yPosDelay: " + yPosDelay);
+  //   // show header
+  //   $header.removeClass("main-header--hidden");
+
+  // } else if(yPosDelay < 150) {
+  //   console.log("second if trigger" + " yPosDelay: " + yPosDelay);
+  //   // hide header
+  //   $header.addClass("main-header--hidden");
+
+  // } else {
+  //   console.log("else trigger" + " yPosDelay: " + yPosDelay);
+  //   // hide header
+  //   $header.addClass("main-header--hidden");
+
+  // } 
+  yPosDelay = window.pageYOffset;
 }
 
 window.addEventListener("scroll", yScroll);
@@ -50,4 +61,4 @@ window.addEventListener("scroll", yScroll);
 
 
 
-});
+// });
